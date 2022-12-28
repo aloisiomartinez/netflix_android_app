993bc71ff19319d5b2c82be2d7a05d8425dff3e9
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-12-28 12:19:00 -0300

    shadow in image

--- app/src/main/java/com/aloisiomartinez/netflixapp/MovieActivity.kt
+++ app/src/main/java/com/aloisiomartinez/netflixapp/MovieActivity.kt
@@ -1,8 +1,12 @@
 package com.aloisiomartinez.netflixapp
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.helper.widget.Layer
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,17 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        // get drawable (layer-list)
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        //get movie
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        //set layer-list to movie
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        //set imageVIew
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
+
     }
 }
