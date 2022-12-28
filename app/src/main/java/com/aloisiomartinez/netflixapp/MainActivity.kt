93552f493f48337fb617b31a9f1366a879d6c5a6
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-12-28 11:01:27 -0300

    recylerView Horizontal for movies categories

--- app/src/main/java/com/aloisiomartinez/netflixapp/MainActivity.kt
+++ app/src/main/java/com/aloisiomartinez/netflixapp/MainActivity.kt
@@ -2,11 +2,9 @@ package com.aloisiomartinez.netflixapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.aloisiomartinez.netflixapp.model.Category
 import com.aloisiomartinez.netflixapp.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -15,15 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movies = mutableListOf<Movie>()
-        for(i in 0 until 60) {
-            val movie = Movie(coverUrl = R.drawable.movie)
-            movies.add(movie)
+        val categories = mutableListOf<Category>()
+        for(j in 0 until 10) {
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until 15) {
+                val movie = Movie(coverUrl = R.drawable.movie)
+                movies.add(movie)
+            }
+            val category = Category("cat $j", movies)
+            categories.add(category)
         }
 
-        val adapter = MainAdapter(movies)
+
+        // Main Adapter na vertical para lista de categorias dos filmes
+        val adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         rv.adapter = adapter
 
     }
