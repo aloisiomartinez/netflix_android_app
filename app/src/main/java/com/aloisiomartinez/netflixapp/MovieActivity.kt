e4617ef0ad60aeb13e7a03569c484cef474e94a9
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-12-29 11:00:43 -0300

    movie detail activity

--- app/src/main/java/com/aloisiomartinez/netflixapp/MovieActivity.kt
+++ app/src/main/java/com/aloisiomartinez/netflixapp/MovieActivity.kt
@@ -4,20 +4,44 @@ import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.helper.widget.Layer
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.aloisiomartinez.netflixapp.model.Movie
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
+        val txtTitle: TextView = findViewById(R.id.movie_txt_title)
+        val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
+        val txtCast: TextView = findViewById(R.id.movie_txt_cast)
+        val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        txtTitle.text = "Batman Begins"
+        txtDesc.text = "Descrição exemplo do filme"
+        txtCast.text = getString(R.string.cast, "Ator A, Ator B, Ator C, Ator D")
+
+        val movies = mutableListOf<Movie>()
+        for(i in 0 until 15) {
+            val movie = Movie(coverUrl = R.drawable.movie)
+            movies.add(movie)
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
+
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
+
 
 
         // get drawable (layer-list)
--- app/src/main/java/com/aloisiomartinez/netflixapp/MovieAdapter.kt
+++ app/src/main/java/com/aloisiomartinez/netflixapp/MovieAdapter.kt
@@ -4,13 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import com.aloisiomartinez.netflixapp.model.Movie
 
-class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+class MovieAdapter(private val movies: List<Movie>, @LayoutRes private val layoutId: Int) :
+    RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
         return MovieViewHolder(view)
     }
 
@@ -23,10 +25,10 @@ class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<Movie
         return movies.size
     }
 
-     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-         fun bind(movie: Movie) {
-             val imageCover: ImageView = itemView.findViewById(R.id.img_cover)
-             imageCover.setImageResource(movie.coverUrl)
-         }
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) {
+            val imageCover: ImageView = itemView.findViewById(R.id.img_cover)
+            imageCover.setImageResource(movie.coverUrl)
+        }
     }
 }
